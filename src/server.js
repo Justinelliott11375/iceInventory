efe44f02c1d9eab5ab9ceae6e45fa417f37cc6be
Justin Elliott <Justinelliott11375@gmail.com>
2020-02-15 17:19:35 -0600

    nodemon fix, heroku test

--- src/server.js
+++ src/server.js
@@ -6,19 +6,21 @@ app.set("port", port);
 
 const server = http.createServer(app);
 
-server.listen(PORT);
+server.listen(port);
 
 function normalizePort(val) {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-      return val;
-    }
-    if (port >= 0) {
-      return port;
-    }
-    return false;
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
   }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
 
 server.on("listening", () => {
-    console.log(`server is listening for requests on port ${server.address().port}`);
+  console.log(
+    `server is listening for requests on port ${server.address().port}`
+  );
 });
