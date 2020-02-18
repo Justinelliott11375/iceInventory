e5e44bbc7fb61698c69cdcd9705c08528b65f59d
Justin Elliott <Justinelliott11375@gmail.com>
2020-02-18 01:36:48 -0600

    report add/show/delete working

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const reportController = require("../controllers/reportController");
 
 router.get("/reports", reportController.index);
+router.get("/reports/new", reportController.new);
+router.post("/reports/create", reportController.create);
+router.get("/reports/:id", reportController.show);
 
 module.exports = router;
