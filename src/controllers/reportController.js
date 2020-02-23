5aa817582e7f4362f4b0f3293883ab4740911733
Justin Elliott <Justinelliott11375@gmail.com>
2020-02-22 22:49:57 -0600

    added console.log to find report index GET error

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,74 +1,74 @@
 const reportQueries = require("../db/queries.reports.js");
 
 module.exports = {
-    index(req, res, next){
-        reportQueries.getAllReports((err, reports) => {
-            if(err) {
-                res.redirect(500, "static/index");
-            } else {
-                res.render("reports/index", {reports});
-            }
-        })
-    },
+  index(req, res, next) {
+    reportQueries.getAllReports((err, reports) => {
+      if (err) {
+        console.log(err);
+        res.redirect(500, "static/index");
+      } else {
+        res.render("reports/index", { reports });
+      }
+    });
+  },
 
-    new(req, res, next){
-        res.render("reports/new");
-    },
+  new(req, res, next) {
+    res.render("reports/new");
+  },
 
-    create(req, res, next){
-        console.log("create called");
-        console.log(req.body);
-        let newReport = {
-            date: req.body.date
-        };
-        reportQueries.addReport(newReport, (err, report) => {
-            if(err){
-                res.redirect(500, "/reports/new");
-            } else {
-                res.redirect(303, `/reports/${report.id}`);
-            }
-        });
-    },
+  create(req, res, next) {
+    console.log("create called");
+    console.log(req.body);
+    let newReport = {
+      date: req.body.date
+    };
+    reportQueries.addReport(newReport, (err, report) => {
+      if (err) {
+        res.redirect(500, "/reports/new");
+      } else {
+        res.redirect(303, `/reports/${report.id}`);
+      }
+    });
+  },
 
-    show(req, res, next){
-        reportQueries.getReport(req.params.id, (err, report) => {
-            if(err || report == null) {
-                res.redirect(404, "/");
-            } else {
-                res.render("reports/show", {report});
-            }
-        })
-    },
+  show(req, res, next) {
+    reportQueries.getReport(req.params.id, (err, report) => {
+      if (err || report == null) {
+        res.redirect(404, "/");
+      } else {
+        res.render("reports/show", { report });
+      }
+    });
+  },
 
-    destroy(req, res, next){
-        reportQueries.deleteReport(req.params.id, (err, report) => {
-            if(err){
-                res.redirect(500, `/reports/${report.id}`)
-            } else {
-                res.redirect(303, "/reports")
-            }
-        });
-    },
+  destroy(req, res, next) {
+    reportQueries.deleteReport(req.params.id, (err, report) => {
+      if (err) {
+        res.redirect(500, `/reports/${report.id}`);
+      } else {
+        res.redirect(303, "/reports");
+      }
+    });
+  },
 
-    edit(req, res, next){
-        reportQueries.getReport(req.params.id, (err, report) => {
-            if(err || report == null){
-                res.redirect(404, "/");
-            } else {
-                res.render("reports/edit", {report});
-            }
-        });
-    },
+  edit(req, res, next) {
+    reportQueries.getReport(req.params.id, (err, report) => {
+      if (err || report == null) {
+        res.redirect(404, "/");
+      } else {
+        res.render("reports/edit", { report });
+      }
+    });
+  },
 
-    update(req, res, next){
-        console.log("report update from controller called");
-        reportQueries.updateReport(req.params.id, req.body, (err, report) => {
-            if(err || report == null){
-                res.redirect(404, `/reports/${req.params.id}/edit`);
-            } else {
-                res.redirect(`/reports/${report.id}`);
-            }
-        })
-    }
-    
-}
+  update(req, res, next) {
+    console.log("report update from controller called");
+    reportQueries.updateReport(req.params.id, req.body, (err, report) => {
+      if (err || report == null) {
+        res.redirect(404, `/reports/${req.params.id}/edit`);
+      } else {
+        res.redirect(`/reports/${report.id}`);
+      }
+    });
+  }
+};
